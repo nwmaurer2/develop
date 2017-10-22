4bb46cd56c37aece0d08f7b3ddede60ef70b4801
danburkhol <danburkhol@gmail.com>
2017-10-22 17:58:10 -0400

    Disable swipe paging

--- app/src/main/java/com/sofwerx/usf/talosconfigurator/MainScreenActivity.java
+++ app/src/main/java/com/sofwerx/usf/talosconfigurator/MainScreenActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,6 +14,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -64,6 +66,10 @@ public class MainScreenActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // Disables swipe paging
+        // https://stackoverflow.com/questions/9650265/how-do-disable-paging-by-swiping-with-finger-in-viewpager-but-still-be-able-to-s/13392198#13392198
+        mViewPager.beginFakeDrag();
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
