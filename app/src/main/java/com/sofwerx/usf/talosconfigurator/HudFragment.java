7993e6f2667ea82e3917092ad66c4a41251fb621
nwmaurer <nwmaurer@mail.usf.edu>
2017-10-23 16:13:06 -0500

    Add ExpandableListViewAdapter class, move previous implementation into HUD fragment

--- app/src/main/java/com/sofwerx/usf/talosconfigurator/HudFragment.java
+++ app/src/main/java/com/sofwerx/usf/talosconfigurator/HudFragment.java
@@ -6,12 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 
 public class HudFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-
+    ExpandableListView expandableListView;
 
     public HudFragment() {
         // Required empty public constructor
@@ -29,7 +30,15 @@ public class HudFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hud, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_hud, container, false);
+        expandableListView = (ExpandableListView) rootView.findViewById(R.id.ExList1);
+
+
+        ExpandableListViewAdapter adapter = new ExpandableListViewAdapter(getActivity().getBaseContext());
+
+        expandableListView.setAdapter(adapter);
+
+        return rootView;
     }
 
 
